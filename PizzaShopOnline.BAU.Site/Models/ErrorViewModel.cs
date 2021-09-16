7d4548cb0995e70a8119aeffe32768e76fab756c
poojarajawat <pooja.rajawat.ext@animalhealth.biz>
2021-09-16 13:57:10 +0530

    Add project files.

---
+++ PizzaShopOnline.BAU.Site/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PizzaShopOnline.BAU.Site.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
