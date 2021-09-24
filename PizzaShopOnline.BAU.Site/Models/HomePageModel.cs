41cb96272a68221e6790905c33907b2274f4899d
poojarajawat <pooja.rajawat.ext@animalhealth.biz>
2021-09-24 13:11:07 +0530

    Added a Model class for HomePage.

---
+++ PizzaShopOnline.BAU.Site/Models/HomePageModel.cs
@@ -0,0 +1,14 @@
+﻿
+namespace PizzaShopOnline.BAU.Site.Models
+{
+    public enum PizzaSize
+    {
+        SMALL_PAN,
+        MEDIUM_PAN,
+        LARGE_PAN,
+    }
+    public class HomePageModel
+    {
+        public PizzaSize Size { get; set; }
+    }
+}
