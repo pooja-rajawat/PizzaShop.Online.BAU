db57168cca4410739c7032647a1e411ac2197f93
Parth8736771 <90830875+Parth8736771@users.noreply.github.com>
2021-09-27 13:57:21 +0530

    3 delivery address page pp (#2)
    * Suceess, Delivery
    
    * Final Change
    
    * Make a new stylesheet "Style.css"
    
    * Update a telephone validation
    
    * Change layout on DeliveryForm
    
    change a site

---
+++ PizzaShopOnline.BAU.Site/Models/Delivery.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PizzaShopOnline.BAU.Site.Models
+{
+    public class Delivery
+    {
+        private const string V = @"decimal{10}";
+
+        [Required(ErrorMessage = "Enter address")]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Enter Name")]
+        [MinLength(2)]
+        [MaxLength(20)]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Enter telephone Number")]
+        [RegularExpression(@"([0-9]{10})$", ErrorMessage = "Not a valid Telephone number")]
+        [Display(Name= "Telephone")]
+        public Double Telephone { get; set; }
+    }
+}
